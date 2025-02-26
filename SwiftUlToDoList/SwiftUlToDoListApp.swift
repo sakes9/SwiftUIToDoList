ee89a1351729932d2fc735cfb639bc381362915d
sake <sake.std.co@gmail.com>
2025-02-26 12:18:12 +0900

    🔧 プロジェクト設定: SwiftData関連のコードを削除する

--- SwiftUlToDoList/SwiftUlToDoListApp.swift
+++ SwiftUlToDoList/SwiftUlToDoListApp.swift
@@ -1,32 +1,10 @@
-//
-//  SwiftUlToDoListApp.swift
-//  SwiftUlToDoList
-//
-//  Created by user on 2025/02/26.
-//
-
 import SwiftUI
-import SwiftData
 
 @main
 struct SwiftUlToDoListApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
