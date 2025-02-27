89522d6dd4e6b170ec6ab2211aa24189cf061fe9
sake <sake.std.co@gmail.com>
2025-02-27 15:13:35 +0900

    💄 UI作成: カスタムリストの作成

--- SwiftUlToDoList/Screens/Views/Main/HomeView.swift
+++ SwiftUlToDoList/Screens/Views/Main/HomeView.swift
@@ -7,16 +7,31 @@ struct HomeView: View {
     @State private var isAddTaskPresented = false // タスク追加アラート表示判定
     @State private var isEditTaskPresented = false // タスク修正アラート表示判定
 
+    // ダミーのToDoタスク構造体
+    private struct DummyTodoTask: Identifiable {
+        var id: UUID // ID
+        var name: String // タスク名
+        var isCompleted: Bool = false // ToDoの完了状態
+    }
+
+    // ダミーのToDoタスクリスト
+    @State private var DUMMY_TODO_TASKS: [DummyTodoTask] = [
+        .init(id: UUID(), name: "タスク1", isCompleted: true),
+        .init(id: UUID(), name: "タスク2", isCompleted: false),
+        .init(id: UUID(), name: "タスク3", isCompleted: false)
+    ]
+
     var body: some View {
-        VStack {
-            ToDoListItem(text: "タスク１", isSelected: true, action: {})
-                .onTapGesture {
-                    isEditTaskPresented = true
-                }
-            ToDoListItem(text: "タスク２", isSelected: false, action: {})
-                .onTapGesture {
-                    isEditTaskPresented = true
-                }
+        CustomList(items: DUMMY_TODO_TASKS, onDelete: onDeleteButtonTapped) { task in
+            ToDoListItem(text: task.name,
+                         isSelected: task.isCompleted,
+                         action: {
+                             print("\(task.name) がタップされました")
+                         })
+                         .listRowInsets(EdgeInsets()) // 要素の余白を削除
+                         .onTapGesture {
+                             isEditTaskPresented = true
+                         }
         }
         // ナビゲーションバー設定
         .navigationBarSetting(title: "ホーム", isVisible: true)
@@ -52,6 +67,12 @@ struct HomeView: View {
     /// タスク追加ボタンタップ時
     private func onTaskAddButtonTapped() {
         isAddTaskPresented = true
+    }
+
+    /// 削除ボタンタップ時
+    /// - Parameter task: タスク
+    private func onDeleteButtonTapped(task: DummyTodoTask) {
+        DUMMY_TODO_TASKS.removeAll { $0.id == task.id }
     }
 
     /// タスク追加
--- SwiftUlToDoList/Screens/Views/Main/TabManageView.swift
+++ SwiftUlToDoList/Screens/Views/Main/TabManageView.swift
@@ -5,20 +5,31 @@ struct TabManageView: View {
     @State private var isAddTabPresented = false // タブ追加アラート表示判定
     @State private var isEditTabPresented = false // タブ修正アラート表示判定
 
+    // ダミーのタブ構造体
+    private struct DummyTodoTab: Identifiable {
+        var id: UUID // ID
+        var name: String // タブ名
+    }
+
+    // ダミーのタブリスト
+    @State private var DUMMY_TODO_TABS: [DummyTodoTab] = [
+        .init(id: UUID(), name: "タブ1"),
+        .init(id: UUID(), name: "タブ2"),
+        .init(id: UUID(), name: "タブ3")
+    ]
+
     var body: some View {
-        VStack {
-            Text("タブ管理画面")
-
-            Text("タブ１")
-                .onTapGesture {
-                    isEditTabPresented = true
-                }
-
-            Text("タブ２")
+        CustomList(items: DUMMY_TODO_TABS, onDelete: onDeleteButtonTapped) { tab in
+            Text(tab.name)
+                .padding(.vertical, 8)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .contentShape(Rectangle()) // タップ領域を確保
                 .onTapGesture {
                     isEditTabPresented = true
                 }
         }
+        .contentMargins(.top, 30) // リスト上部の余白
+
         // ナビゲーションバー設定
         .navigationBarSetting(title: "タブ管理", isVisible: true)
         .navigationBarIconSetting(name: "plus", isEnabled: true, action: onTabAddIconTapped)
@@ -47,6 +58,12 @@ struct TabManageView: View {
     /// タブ追加アイコンタップ時
     private func onTabAddIconTapped() {
         isAddTabPresented = true
+    }
+
+    /// 削除ボタンタップ時
+    /// - Parameter tab: タブ
+    private func onDeleteButtonTapped(tab: DummyTodoTab) {
+        DUMMY_TODO_TABS.removeAll { $0.id == tab.id }
     }
 
     /// タブ追加
