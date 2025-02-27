a72bff4677bd6fd94d88f9292e9fdc22f57e0ed4
sake <sake.std.co@gmail.com>
2025-02-27 14:12:32 +0900

    💄 UI作成: テキスト入力アラートの作成

--- SwiftUlToDoList/Screens/Views/Main/HomeView.swift
+++ SwiftUlToDoList/Screens/Views/Main/HomeView.swift
@@ -4,10 +4,19 @@ import SwiftUI
 struct HomeView: View {
     @Binding var navigationPath: [NavigationItem]
 
+    @State private var isAddTaskPresented = false // タスク追加アラート表示判定
+    @State private var isEditTaskPresented = false // タスク修正アラート表示判定
+
     var body: some View {
         VStack {
             ToDoListItem(text: "タスク１", isSelected: true, action: {})
+                .onTapGesture {
+                    isEditTaskPresented = true
+                }
             ToDoListItem(text: "タスク２", isSelected: false, action: {})
+                .onTapGesture {
+                    isEditTaskPresented = true
+                }
         }
         // ナビゲーションバー設定
         .navigationBarSetting(title: "ホーム", isVisible: true)
@@ -19,6 +28,20 @@ struct HomeView: View {
 
         // コンポーネント
         .floatingButton(iconName: "plus", action: onTaskAddButtonTapped)
+        .textFieldAlert(isPresented: $isAddTaskPresented,
+                        title: "ToDo追加",
+                        message: "追加するタスク名を入力してください\n(50文字以内)",
+                        placeholder: "例）アプリ開発",
+                        defaultText: "",
+                        maxLength: 50,
+                        onConfirm: addTask)
+        .textFieldAlert(isPresented: $isEditTaskPresented,
+                        title: "ToDo修正",
+                        message: "修正するタスク名を入力してください\n(50文字以内)",
+                        placeholder: "例）アプリ開発",
+                        defaultText: "タスク",
+                        maxLength: 50,
+                        onConfirm: editTask)
     }
 
     /// タブ管理アイコンタップ時
@@ -28,7 +51,19 @@ struct HomeView: View {
 
     /// タスク追加ボタンタップ時
     private func onTaskAddButtonTapped() {
-        print("タスク追加ボタンがタップされました")
+        isAddTaskPresented = true
+    }
+
+    /// タスク追加
+    /// - Parameter text: 入力テキスト
+    private func addTask(text: String) {
+        print("タスク追加: \(text)")
+    }
+
+    /// タスク修正
+    /// - Parameter text: 入力テキスト
+    private func editTask(text: String) {
+        print("タスク修正: \(text)")
     }
 }
 
--- SwiftUlToDoList/Screens/Views/Main/TabManageView.swift
+++ SwiftUlToDoList/Screens/Views/Main/TabManageView.swift
@@ -2,17 +2,63 @@ import SwiftUI
 
 /// タブ管理画面
 struct TabManageView: View {
+    @State private var isAddTabPresented = false // タブ追加アラート表示判定
+    @State private var isEditTabPresented = false // タブ修正アラート表示判定
+
     var body: some View {
         VStack {
             Text("タブ管理画面")
+
+            Text("タブ１")
+                .onTapGesture {
+                    isEditTabPresented = true
+                }
+
+            Text("タブ２")
+                .onTapGesture {
+                    isEditTabPresented = true
+                }
         }
         // ナビゲーションバー設定
         .navigationBarSetting(title: "タブ管理", isVisible: true)
-        .navigationBarIconSetting(name: "plus", isEnabled: true, action: {})
+        .navigationBarIconSetting(name: "plus", isEnabled: true, action: onTabAddIconTapped)
 
         // 画面スタイル
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color("BgColor"))
+
+        // コンポーネント
+        .textFieldAlert(isPresented: $isAddTabPresented,
+                        title: "タブ追加",
+                        message: "追加するタブ名を入力してください\n(20文字以内)",
+                        placeholder: "例）勉強",
+                        defaultText: "",
+                        maxLength: 20,
+                        onConfirm: addTab)
+        .textFieldAlert(isPresented: $isEditTabPresented,
+                        title: "タブ修正",
+                        message: "修正するタブ名を入力してください\n(20文字以内)",
+                        placeholder: "例）勉強",
+                        defaultText: "タブ",
+                        maxLength: 20,
+                        onConfirm: editTab)
+    }
+
+    /// タブ追加アイコンタップ時
+    private func onTabAddIconTapped() {
+        isAddTabPresented = true
+    }
+
+    /// タブ追加
+    /// - Parameter text: 入力テキスト
+    private func addTab(text: String) {
+        print("タブ追加: \(text)")
+    }
+
+    /// タブ修正
+    /// - Parameter text: 入力テキスト
+    private func editTab(text: String) {
+        print("タブ修正: \(text)")
     }
 }
 
