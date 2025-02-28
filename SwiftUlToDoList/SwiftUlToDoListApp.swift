1bcbff9d934536eba8d65fb4c6bf28a80870b4da
sake <sake.std.co@gmail.com>
2025-02-28 12:14:18 +0900

    ✨ 機能作成: SwiftDataサービスの作成

--- SwiftUlToDoList/SwiftUlToDoListApp.swift
+++ SwiftUlToDoList/SwiftUlToDoListApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct SwiftUlToDoListApp: App {
+    let modelContainer = SwiftDataService.shared.getModelContainer() // モデルコンテナ
+
     var body: some Scene {
         WindowGroup {
             InitialView()
         }
+        .modelContainer(modelContainer)
     }
 }
